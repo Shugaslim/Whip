3418bb69f424ec6e4fff3f64bdbe96012653617f
Shugaslim <ChrisMadison13@gmail.com>
2015-11-22 12:39:10 -0600

    First Commit of Whip
    A game that teaches people the whip dance using unity and myo bands

---
+++ Assets/beatRepresentation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beatRepresentation : MonoBehaviour {
+
+	private GameObject boom;
+	private SpriteRenderer bam;
+	float bps = (float)67.5/60;
+
+
+
+	// Use this for initialization
+	void Start () {
+		boom = this.gameObject;
+		bam = boom.GetComponent<SpriteRenderer>();
+		bam.color = Color.white;
+		InvokeRepeating("tileColor", (float)1.0, (1/bps));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void tileColor()
+	{
+		if(bam.color == Color.white)
+			bam.color = Color.red;
+		else
+			bam.color = Color.white;
+	}
+
+	IEnumerator TileChange()
+	{
+		yield return new WaitForSeconds(1);
+		tileColor();
+	}
+}
---
+++ Assets/checkRythm.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Pose = Thalmic.Myo.Pose;
+using Arm = Thalmic.Myo.Arm;
+using XDirection = Thalmic.Myo.XDirection;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class checkRythm : MonoBehaviour{
+
+	GameObject img;
+	SpriteRenderer sprit;
+
+	public GameObject myo;
+
+	public float roll = 0.0f;
+
+
+	float bps = (float)135 / 60;
+	protected int Highscore = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		img = this.gameObject;
+		sprit = img.GetComponent<SpriteRenderer> ();
+		sprit.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("it works");
+
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		Vector3 zeroRoll = computeZeroRollVector (myo.transform.forward);
+		float roll = rollFromZero (zeroRoll, myo.transform.forward, myo.transform.up);
+
+		if(Time.time % bps < 0.5 && roll < 0) {
+			Highscore++;
+			sprit.enabled = true;
+			thalmicMyo.Vibrate(VibrationType.Medium);
+		} else {
+			sprit.enabled = false;
+		}
+	}
+	float normalizeAngle (float angle)
+	{
+		if (angle > 180.0f) {
+			return angle - 360.0f;
+		}
+		if (angle < -180.0f) {
+			return angle + 360.0f;
+		}
+		return angle;
+	}
+
+	Vector3 computeZeroRollVector (Vector3 forward)
+	{
+		Vector3 antigravity = Vector3.up;
+		Vector3 m = Vector3.Cross (myo.transform.forward, antigravity);
+		Vector3 roll = Vector3.Cross (m, myo.transform.forward);
+		
+		return roll.normalized;
+	}
+
+	float rollFromZero (Vector3 zeroRoll, Vector3 forward, Vector3 up)
+	{
+
+		float cosine = Vector3.Dot (up, zeroRoll);
+
+		Vector3 cp = Vector3.Cross (up, zeroRoll);
+		float directionCosine = Vector3.Dot (forward, cp);
+		float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
+		
+		// Return the angle of roll (in degrees) from the cosine and the sign.
+		return sign * Mathf.Rad2Deg * Mathf.Acos (cosine);
+	}
+	
+
+}
